f91761a798ece274e1e1f13ebd018458310c43a1
pritam-raskar <pritam.raskar@ounterage.com>
2025-01-11 11:02:51 -0500

    Cais banner added

--- cais-alert-service/src/main/java/com/dair/cais/CaisAlertApplication.java
+++ cais-alert-service/src/main/java/com/dair/cais/CaisAlertApplication.java
@@ -34,38 +34,3 @@ public class CaisAlertApplication {
 		}
 	}
 }
-
-
-
-//package com.dair.cais;
-//
-//import  com.dair.cais.common.config.JpaConfig;
-//import com.dair.cais.common.config.DataSourceConfig;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@SpringBootApplication
-//@EntityScan(basePackages = "com.dair.cais")
-//@Import({JpaConfig.class, DataSourceConfig.class})
-//public class CaisAlertApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(CaisAlertApplication.class, args);
-//	}
-//
-//	@Configuration
-//	public static class WebConfig implements WebMvcConfigurer {
-//		@Override
-//		public void addCorsMappings(CorsRegistry registry) {
-//			registry.addMapping("/**")
-//					.allowedOrigins("*")
-//					.allowedHeaders("*")
-//					.allowedMethods("*");
-//		}
-//	}
-//}
